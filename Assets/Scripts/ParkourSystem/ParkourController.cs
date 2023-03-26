e3bdd7d9c6f3a4df9244f6eb396c858ad54f1246
HDRP <hdrp0720@gmail.com>
2023-03-26 21:50:54 +0900

    start project

---
+++ Assets/Scripts/ParkourSystem/ParkourController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkourController : MonoBehaviour
+{
+    [SerializeField] List<ParkourAction> parkourActions;
+    [SerializeField] ParkourAction preFalling;
+    [SerializeField] float autoDropHeightLimit = 1f;
+
+    bool inAction;
+
+    EnvironmentScanner environmentScanner;
+    Animator animator;
+    PlayerController playerController;
+
+    private void Awake()
+    {
+        environmentScanner = GetComponent<EnvironmentScanner>();
+        animator = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        var hitData = environmentScanner.ObstacleCheck();
+        if (Input.GetButton("Jump") && !inAction)
+        {            
+            if (hitData.forwardHitFound)
+            {
+                // Debug.Log("Obstacle Found" + hitData.forwardHit.transform.name);
+                foreach (var action in parkourActions)
+                {
+                    if (action.CheckIfPossible(hitData, transform))
+                    {
+                        StartCoroutine(DoParkourAction(action));
+                        break;
+                    }
+                }              
+            }
+        }
+
+        if (playerController.IsOnLedge && !inAction && !hitData.forwardHitFound)
+        {
+            bool shouldJump = true;
+            if (playerController.LedgeData.height > autoDropHeightLimit && !Input.GetButton("Jump"))
+                shouldJump = false;
+
+            if (shouldJump && playerController.LedgeData.angle <= 50)
+            {
+                playerController.IsOnLedge = false;
+                StartCoroutine(DoParkourAction(preFalling));
+            }           
+        }
+    }
+
+    IEnumerator DoParkourAction(ParkourAction action)
+    {
+        inAction = true;
+        playerController.SetControl(false);
+
+        animator.SetBool("mirrorAction", action.Mirror);
+        animator.CrossFade(action.AnimName, 0.2f);
+        yield return null; // wait for the next frame
+
+        var animState = animator.GetNextAnimatorStateInfo(0);
+        if (!animState.IsName(action.AnimName))
+            Debug.LogError("The parkour animation name is wrong!");
+
+        //yield return new WaitForSeconds(animState.length);
+
+        float timer = 0f;
+        while (timer <= animState.length)
+        {
+            timer += Time.deltaTime;
+           
+            if (action.RotateToObstacle)
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, action.TargetRotation, playerController.RotationSpeed * Time.deltaTime);
+
+            if (action.EnableTargetMatching)
+                MatchTarget(action);
+
+            if (animator.IsInTransition(0) && timer > 0.5f)
+                break;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(action.PostActionDelay);
+
+        playerController.SetControl(true);
+        inAction = false;
+    }
+
+    void MatchTarget(ParkourAction action)
+    {
+        if (animator.isMatchingTarget)
+            return;
+
+        animator.MatchTarget(action.MatchPos, transform.rotation, action.MatchBodyPart, new MatchTargetWeightMask(action.MatchPosWeight, 0), action.MatchStartTime, action.MatchTargetTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+# Parkour System
+[![Cover](./Assets/Art/Samples/cover-parkour.png)](https://youtu.be/aHVScQy3nrs)
+
+## Camera Controller & Player Controller
+- Camera Controller
+  - followTarget
+  - distance
+  - minVerticalAngle & maxVerticalAngle
+  - framingOffset
+  - invertX & invertY
+
+- Player Controller
+  - moveSpeed
+  - rotationSpeed
+  - Gravity & Ground Check
+    - groundCheckRadius
+    - groundCheckOffset
+    - groundLayer
+
+## Character Controller
+- Skin Width 값 = Radius 값의 10%를 값으로 할당
+- Center Y값 = (Height / 2) + Skin Width 값으로 할당
+
+> Skin Width: 충돌 판정시의 Jittering과 stuck 상태를 줄일 때 조절하여 사용
+
+## Ground Check
+유니티내의 CharacterController의 IsGrounded를 사용 할수 있으나, 종종 다른 판정이 발생하며, 실제 유니티에서 만든 [스타터 에셋-TPS](https://assetstore.unity.com/packages/essentials/starter-assets-third-person-character-controller-196526)에서도 직접 체크하고 있는걸 확인 할 수 있다.
+
+> groundCheckRadius = Character Controller내의 radius 값이 되도록 유지 필요
+
+## Environment Scanner
+다중 RayCast를 사용하여 플레이어 앞의 장애물 및 높이 정보를 확인
+
+## Root motion & Animations CrossFade
+애니메이션간의 전환상태에 이상이 생길 수 있기에 비슷한 프레임으로 유지하거나 exit time을 조정해야 한다.
+
+## Target Matching
+- Target Matching
+  - EnableTargetMatching : 타겟매칭 애니메이션을 적용할지 말지 선택
+  - matchBodyPart : 타겟매칭을 적용시킬 부위 선택 (루트-몸통-(왼/오)팔-(왼/오)다리)
+  - matchStartTime : 타겟 매칭이 시작될 프레임, %로 적용 `(예. 31% 지점 -> 0.31)`
+  - matchTargetTime : 타겟 매칭이 적용될 프레임, %로 적용 `(예. 51% 지점 -> 0.31)`
+
+- Mirror Animation
+
+## Resources from...
+- mixamo character 
+  - Erika Archer
+- mixamo animation
+  - Idle
+  - Walking
+  - Running
+  - Jumping
+  - Running Jumping
+  - Sprint To Wall Climb
+  - Crouched To Standing
+  - Jumping Fence
+  - Jumping 2ft
+  - Fall A Loop
+  - Fall A Land To Standing Idle
+
+## References
+* https://assetstore.unity.com/packages/essentials/starter-assets-third-person-character-controller-196526
+* https://docs.unity3d.com/ScriptReference/CharacterController-skinWidth.html
